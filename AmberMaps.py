5bb734dce4579002ee29b38dcd9676867e90a9d0
shaoqx <shaoqz@icloud.com>
2020-09-15 10:46:32 +0800

    Fixed HIP problem in mapping

--- AmberMaps.py
+++ AmberMaps.py
@@ -17,6 +17,6 @@ Map residue key with index number
 
 Resi_map={'R':'ARG','H':'HIS','K':'LYS','D':'ASP','E':'GLU','S':'SER','T':'THR','N':'ASN','Q':'GLN','C':'CYS','U':'SEC','G':'GLY','P':'PRO','A':'ALA','V':'VAL','I':'ILE','L':'LEU','M':'MET','F':'PHE','Y':'TYR','W':'TRP'}
 
-Resi_map2={'ARG':'R','HIS':'H','LYS':'K','ASP':'D','GLU':'E','SER':'S','THR':'T','ASN':'N','GLN':'Q','CYS':'C','SEC':'U','GLY':'G','PRO':'P','ALA':'A','VAL':'V','ILE':'I','LEU':'L','MET':'M','PHE':'F','TYR':'Y','TRP':'W'}
+Resi_map2={'ARG':'R','HIS':'H','HIE':'H','HIP':'H','HID':'H','LYS':'K','ASP':'D','GLU':'E','SER':'S','THR':'T','ASN':'N','GLN':'Q','CYS':'C','SEC':'U','GLY':'G','PRO':'P','ALA':'A','VAL':'V','ILE':'I','LEU':'L','MET':'M','PHE':'F','TYR':'Y','TRP':'W'}
 
 Resi_list=['R','H','K','D','E','S','T','N','Q','C','U','G','P','A','V','I','L','M','F','Y','W']
