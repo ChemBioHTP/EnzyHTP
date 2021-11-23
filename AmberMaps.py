09440980af473904721ea3a18deba37036e4af53
shaoqx <shaoqz@icloud.com>
2021-11-22 19:56:00 -0600

    Update: add netural to subgroups

--- AmberMaps.py
+++ AmberMaps.py
@@ -770,12 +770,13 @@ tip3p_metal_map = {     'AL': {'AL': ['Al3+', '1.369', '0.01128487']},
 # ===============================================
 # Residue catageries (subgroups)
 resi_subgrp = {
-      'polar' : ['R','H','K','D','E','S','T','N','Q','C','Y'],
-      'charged' : ['R','H','K','D','E'],
-      'positive' : ['R','H','K'],
-      'negative' : ['D','E'],
-      'polarNcharge' : ['S','T','N','Q','C','Y'],
-      'nonpolar' : ['A','V','I','L','P','M','F','W','G']
+      'polar' :         ['R','H','K','D','E','S','T','N','Q','C','Y'],
+      'charged' :       ['R','H','K','D','E'],
+      'positive' :      ['R','H','K'],
+      'negative' :      ['D','E'],
+      'polarNcharge' :  ['S','T','N','Q','C','Y'],
+      'nonpolar' :      ['A','V','I','L','P','M','F','W','G'],
+      'netural' :       ['S','T','N','Q','C','Y','A','V','I','L','P','M','F','W','G']
 }
 # Residue Volume Rank (From http://www.imgt.org/IMGTeducation/Aide-memoire/_UK/aminoacids/abbreviation.html)
 resi_vol_rank = ['G','A','S','C','D','P','N','T','E','V','Q','H','M','I','L','K','R','F','Y','W']
