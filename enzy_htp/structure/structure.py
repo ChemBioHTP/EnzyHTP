6430055d0c68cf4ba1d557c7a5a028a1ed64c739
Chris Jurich <chris.jurich@vanderbilt.edu>
2022-02-08 16:44:47 -0600

    working on re-factoring structure class

---
+++ enzy_htp/structure/structure.py
@@ -0,0 +1,43 @@
+"""Defines a structure class that serves as a parent class for different types of proteins.
+
+Author: Chris Jurich, <chris.jurich@vanderbilt.edu>
+Date: 2022-02-01
+"""
+
+from biopandas.pdb import PandasPdb
+
+class Structure(PandasPdb):
+
+	def __init__(self):
+		# TODO make residues and chains as well
+		pass
+#		self.sequence_ = kwargs.get('sequence', None)
+#		self.structure_ = kwargs.get('structure', None)
+
+
+	def __eq__( self, other ):
+		return self.sequence_ == other.sequence_ 
+
+
+
+	def rm_wat( self ):
+		# TODO put a deprecation warning
+		self.rm_solvent()
+
+	def rm_solvent( self ):
+		print(self.df.keys())
+		def clean_df( key_name ):
+			SKIP=["Na+", "Cl-", "WAT", "HOH"]
+			sliced_df = self.df[key_name]
+			print(sliced_df)
+			self.df[key_name] = sliced_df[
+				(~sliced_df.atom_name.isin(SKIP))&(sliced_df.record_name!='TER')
+			]
+
+		clean_df( 'ATOM' )
+		#clean_df( 'OTHERS' )
+
+		pass
+
+	def get_protonation( self ):
+		pass
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt', 'r') as f:
+	requirements = f.read().splitlines()
+
+
+setup(name='enzy_htp',
+        version='0.1.0',
+        description='TODO',
+        author='Chris Jurich',
+        author_email='cjurich2@huskers.unl.edu',
+        packages=['enzy_htp',
+		'enzy_htp.structure',
+		],
+		install_requirements=requirements
+        )
