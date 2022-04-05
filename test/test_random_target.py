8a573d29c5a8971f97e14fca308a41ba85f0fecf
Xinchun Ran <xinchun@accre.vu>
2022-04-05 13:53:41 -0500

    test_4_random_target

---
+++ test/test_random_target.py
@@ -0,0 +1,11 @@
+from random import choice
+from Class_PDB import *
+from AmberMaps import Resi_map2
+
+
+def test_random_mut_generator():
+
+
+    facd_test_obj = Class_PDB.PDB('../Test_file/FAcD/FAcD_FA_ASP.pdb')
+    facd_test_obj.random_mut_generator(3, A)
+    assert 
