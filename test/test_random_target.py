36126f7c2d949d7a13cb7c596e13877fb7433a37
Xinchun Ran <xinchun@accre.vu>
2022-04-11 23:33:23 -0500

    the random generator and the test

--- test/test_random_target.py
+++ test/test_random_target.py
@@ -4,8 +4,8 @@ from AmberMaps import Resi_map2
 
 
 def test_random_mut_generator():
-
-
     facd_test_obj = Class_PDB.PDB('../Test_file/FAcD/FAcD_FA_ASP.pdb')
-    facd_test_obj.random_mut_generator(3, A)
-    assert 
+    facd_test_obj.random_mut_generator(12, 'A')
+
+
+print(test_random_mut_generator())
