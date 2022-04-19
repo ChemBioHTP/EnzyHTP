c44d31f14e21c51456d9133153fdc7c349c6a39f
KleinesMesser <73961654+KleinesMesser@users.noreply.github.com>
2022-04-19 14:21:23 -0500

    Update settings.py

--- enzy_htp/core/settings.py
+++ enzy_htp/core/settings.py
@@ -7,11 +7,11 @@ from collections import namedtuple
 
 EnzyHTPVersion = namedtuple('EnzyHTPVersion', 'major minor patch')
 
-_VERSION = None #TODO(CJ): fill in the version here with major=0, minor=1 and patch = 0
+_VERSION = EnzyHTPVersion(major = 0, minor = 1,  patch = 0) #TODO(CJ): fill in the version here with major=0, minor=1 and patch = 0
 
 def get_version() -> EnzyHTPVersion:  # should return version named tuple
     """Method that gets the current EnzyHTPVersion from the module."""
-    pass
+    return _VERSION
 
 def version_to_str(version : EnzyHTPVersion) -> str:
     """Method that takes an EnzyHTPVersion and converts it to a str in the format "major.minor.patch".
@@ -19,15 +19,20 @@ def version_to_str(version : EnzyHTPVersion) -> str:
 	>>> version = EnzyHTPVersion(major=1.5, minor=3.2, patch=8.9)
 	>>> version_to_str( version ) = "1.3.8"
 	"""
-    pass
+    return str(int(version.major)) + '.' + str(int(version.minor)) + '.' + str(int(version.patch))
 
 def data_dir() -> str:
     """Returns a path to the data directory at enzy_htp/data/"""
-    pass
+    return 'enzy_htp/data/'
 
 # returns path to a data/ directory in enzy_htp/data/
 
 def is_compatible_os() -> bool:
     """Checks if the operating system is compatible. Should fail if operating system is not linux or mac-os"""
 	#HINT(CJ): Use sys.platform
-    pass
+    import sys
+    system = sys.platform
+    if system in ['linux', 'darwin']:
+       return True
+    else:
+       return False
