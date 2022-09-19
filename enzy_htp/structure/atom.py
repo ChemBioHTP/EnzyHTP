825b76a1154ac84d5c393ff8d40e789d5226ddc5
shaoqz <shaoqz@icloud.com>
2022-09-19 17:37:48 -0500

    Bugfix: fixed optional attrs

--- enzy_htp/structure/atom.py
+++ enzy_htp/structure/atom.py
@@ -6,11 +6,13 @@ Author: Chris Jurich <chris.jurich@vanderbilt.edu>
 Date: 2022-03-19
 '''
 from __future__ import annotations
+import sys
 from typing import Tuple
 
 import numpy as np
 import pandas as pd
 from enzy_htp.core.doubly_linked_tree import DoubleLinkNode
+from enzy_htp.core.logger import _LOGGER
 
 
 class Atom(DoubleLinkNode):
@@ -42,16 +44,16 @@ class Atom(DoubleLinkNode):
         self._b_factor = None
         self._element = None
         self._charge = None
-
-        ds_items = ds.items()
-        if 'atom_number' in ds_items:
+        ds_keys = ds.keys()
+        if 'atom_number' in ds_keys:
             self._idx = ds['atom_number']
-        if 'b_factor' in ds_items:        
+        if 'b_factor' in ds_keys:
             self._b_factor = ds['b_factor']
-        if 'element_symbol' in ds_items:
+        if 'element_symbol' in ds_keys:
             self._element = ds['element_symbol'].strip()
-        if 'charge' in ds_items and not np.isnan(ds['charge']):
+        if 'charge' in ds_keys and not np.isnan(ds['charge']):
             self._charge = float(ds['charge'])
+
 
     #region === Getter-Attr (ref) ===
     @property
@@ -65,6 +67,8 @@ class Atom(DoubleLinkNode):
     @property
     def idx(self) -> int:
         """Getter for the Atom()'s index."""
+        if self._idx == None:
+            _LOGGER.warning(f'{sys._getframe().f_back.f_back.f_code.co_name}:{sys._getframe().f_back.f_code.co_name} is using index of an non-indexing atom: {self}')
         return self._idx
     @idx.setter
     def idx(self, val):
@@ -110,3 +114,9 @@ class Atom(DoubleLinkNode):
     def charge(self, val):
         self._charge = val
     #endregion
+
+    #region == Special ==
+    def __str__(self):
+        return f'Atom({self._name}, {self._idx}, {self._coord}, {self._parent}, {self._b_factor}, {self._element}, {self._charge} )'
+    #endregion
+
