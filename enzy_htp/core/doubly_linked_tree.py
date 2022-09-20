d7ec4a637c20ed58e5bfef7da0b2c72ba5a2446b
shaoqz <shaoqz@icloud.com>
2022-09-20 14:38:50 -0500

    Update: addressed reviewer's comments

--- enzy_htp/core/doubly_linked_tree.py
+++ enzy_htp/core/doubly_linked_tree.py
@@ -1,39 +1,48 @@
-"""abstract some data structure operation:
-set_parent
-set_child
-copy
-deepcopy
+"""This class summarized data structure level operation from Structure/Chain/Residue/Atom, which
+are designed to contain data in the doublelinked manner, that is, a parent object holds a list
+of children objects while each children object also holds a reference of the parent object. (e.g:
+Chain().residues[0].chain is the first Chain object)
+
+Common operations from such summarization are:
+set/get_parent (set_ghost_children for leafs)
+set/get_children (set_ghost_parent for the root, these are designed so that the summarized class
+                  know what to expect.)
+__deepcopy__
+__getitem__
+__delitem__
+__len__
 
 Author: QZ Shao, <shaoqz@icloud.com>
 Date: 2022-09-14
 """
 
-from typing import List
+from typing import Any, Dict, List, Union
 
 
 class DoubleLinkNode():
-    '''
+    """
     class for parent objects of the doubly linked tree
-    '''
+    """
     # === Attr ===
     # parent use
     def set_children(self, children: List):
-        '''
+        """
         set children and add self as parent of children
-        '''
+        """
         self._children = children
         for child in self._children:
             child.parent = self
 
     def set_ghost_children(self):
-        '''
+        """
         method for the node with no children to set
         The idea is DoubleLinkNode defines what to set when no children
-        '''
+        """
         self._children = None
 
     def get_children(self) -> List:
         return self._children
+
     # api out of class use
     @property
     def children(self) -> List:
@@ -44,21 +53,22 @@ class DoubleLinkNode():
 
     # child use
     def set_parent(self, parent):
-        '''
+        """
         only set parent to self
-        * do not add self to parent's children
-        '''
+        * do not add self to parent"s children
+        """
         self._parent = parent
-    
+
     def set_ghost_parent(self):
-        '''
+        """
         method for the node with no parent to set
         The idea is DoubleLinkNode defines what to set when no parent
-        '''
+        """
         self._parent = None
 
     def get_parent(self):
         return self._parent
+
     # api out of class use
     @property
     def parent(self) -> List:
@@ -69,10 +79,12 @@ class DoubleLinkNode():
 
 
     # === copy ===
-    def __deepcopy__(self, memo):
-        '''
+    def __deepcopy__(self, memo: Union[Dict[int, Any], None]):
+        """
         support deepcopy of the object
-        '''
+        shallow copy wont cause any problem but deepcopy do as explained on:
+        https://docs.python.org/3.10/library/copy.html?highlight=deepcopy
+        """
         pass
 
     # === special ===
@@ -83,5 +95,5 @@ class DoubleLinkNode():
         del self._children[key]
 
     def __len__(self) -> int:
-        return len(self._children)        
+        return len(self._children)
 
