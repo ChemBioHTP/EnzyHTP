a53c642d8cf1cfb6d628580251e7d48bbdae2762
shaoqz <shaoqz@icloud.com>
2022-09-21 00:13:01 -0500

    design science api for geno app

---
+++ test/target_genoenzyhtp.py
@@ -0,0 +1,21 @@
+# type: ignore (for pylance)
+'''
+Scratch for what we want the final EnzyHTP API to be. (Use for design)
+
+Author: Qianzhen (QZ) Shao, <shaoqz@icloud.com>
+Date: 2022-08-30
+'''
+
+# pylint: disable=all, E
+import enzy_htp as eh
+
+stru_parser = eh.structure.structure_io.PDBParser()
+
+def geno_enzy_htp_workflow(stru_parser: StructureParser):
+    pass
+
+
+
+
+
+
