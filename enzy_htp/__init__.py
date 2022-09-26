5c713073eb04f8eef182a3eaabefc34e1ff908dd
shaoqz <shaoqz@icloud.com>
2022-09-26 11:09:36 -0500

    Update: moved __init__ over from KH_dist

--- enzy_htp/__init__.py
+++ enzy_htp/__init__.py
@@ -1,27 +1,47 @@
-"""
+"""EnzyHTP is a holistic platform that allows high-throughput molecular simulation of enzymes. 
+Molecular simulations, such as quantum mechanics (QM), molecular mechanics (MM), and QM/MM modeling, 
+are highly applicable to the design and discovery of new biocatalysts. Molecular simulations provide 
+time-resolved, atomic and electronic detail for enzymatic reactions, complementing experimental sequence 
+and structure-level information. As such, expanding molecular simulation data can improve the predictive 
+power of machine learning models to evaluate mutation effects in enzyme catalysis. However, large-scale 
+collection of molecular simulation data presents a significant challenge due to complex demands. 
+To build an enzyme model appropriate for simulations, multiple hierarchies of structural definitions and 
+treatments must be established such as protein stoichiometry, binding site, predicting amino acid protonation 
+state, addition of missing residues, performing an amino acid substitution, and creating reacting species. 
+Most enzyme modeling practices use similar structural operations but rely on manual curation, which is 
+highly inefficient and hampers reproducibility. EnzyHTP, a high-throughput enzyme simulation tool, bypasses 
+these issues through automation of molecular model construction, mutation, sampling and energy calculation.
+
+
+Author: Qianzhen (QZ) Shao <qianzhen.shao@vanderbilt.edu>
 Author: Chris Jurich <chris.jurich@vanderbilt.edu>
+
 
 Date: 2022-06-26
 """
-# python imports
-import sys
-import logging
-from typing import Tuple
 
-
-# from .interface import Interface
 from .structure import Structure, Residue
 from .core import (
     EnvironmentManager,
     MissingEnvironmentElement,
     InvalidResidueCode,
     _LOGGER,
-	write_data
+    write_data,
 )
 from .preparation import PDBLine, PDBPrepper, read_pdb_lines
 from .chemical import ResidueType
+from ._config import Config
+# from ._interface import Interface
+# from .mutation import mutate_pdb, restriction_object, MutationRestrictions
+# from .geometry import sample_geometries
 
-from .molecular_mechanics import AmberInterface
-from .quantum_mechanics import GaussianInterface, MultiwfnInterface
+config = Config()
+"""
+the singleton Config() object for accessing all configurations in EnzyHTP
+"""
 
-from .mutation import  MutationRestrictions, restriction_object, mutate_pdb
+# interface = Interface(config)
+# """
+# the singleton interface object that provide access to all singleton interface objects
+# """
+
