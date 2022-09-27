d4fee8180cd8dbf12232c84ce13b38f73b3f475d
shaoqz <shaoqz@icloud.com>
2022-09-26 23:13:20 -0500

    Update: add remove_non_peptide

--- test/structure/structure_operation/test_general.py
+++ test/structure/structure_operation/test_general.py
@@ -10,21 +10,29 @@ import enzy_htp.structure.structure_operation as stru_oper
 
 #_LOGGER.setLevel(logging.DEBUG)
 CURRDIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = f'{CURRDIR}/../data/'
+DATA_DIR = f"{CURRDIR}/../data/"
 sp = PDBParser()
 
 def test_remove_solvent():
-    pdb_file_path = f'{DATA_DIR}1Q4T_ligand_test.pdb'
+    pdb_file_path = f"{DATA_DIR}1Q4T_ligand_test.pdb"
     stru: Structure = sp.get_structure(pdb_file_path)
 
     stru_oper.remove_solvent(stru)
     assert len(stru.solvents) == 0
 
 def test_remove_empty_chain():
-    pdb_file_path = f'{DATA_DIR}1Q4T_ligand_test.pdb'
+    pdb_file_path = f"{DATA_DIR}1Q4T_ligand_test.pdb"
     stru: Structure = sp.get_structure(pdb_file_path)
 
-    stru[1].children = []
+    stru[1].residues = []
     stru_oper.remove_empty_chain(stru)
     assert len(stru) == 3
 
+def test_remove_non_peptide():
+    pdb_file_path = f"{DATA_DIR}1Q4T_ligand_test.pdb"
+    stru: Structure = sp.get_structure(pdb_file_path)
+    stru_oper.remove_non_peptide(stru)
+    assert tuple(map(lambda x: x.name, stru)) == ("A", "B")
+
+def test_update_residues():
+    pass
