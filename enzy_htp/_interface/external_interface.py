adfd292ae946f495e7bff3a51ec8bdf78a063be6
shaoqz <shaoqz@icloud.com>
2022-10-05 19:23:08 -0500

    a place holder

---
+++ enzy_htp/_interface/external_interface.py
@@ -0,0 +1,14 @@
+"""Class for unified functions of classes interfacing external softwares.
+
+Author: Qianzhen (QZ) Shao <qianzhen.shao@vanderbilt.edu>
+Author: Chris Jurich <chris.jurich@vanderbilt.edu>
+
+Date: 2022-09-25
+"""
+
+# class ExternalInterface():
+
+#     def __init__(self, config: InterfaceConfig = None) -> None:
+#         self._config = config
+#         if not self._config:
+#             self._config = 
