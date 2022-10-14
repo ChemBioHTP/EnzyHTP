bb0fa8bb71e9f4dca08de6e09638eaf1f048e7ac
Chris Jurich <chris.jurich@vanderbilt.edu>
2022-10-14 17:44:45 -0500

    quick change

--- enzy_htp/structure/structure_io/pdb_io.py
+++ enzy_htp/structure/structure_io/pdb_io.py
@@ -673,7 +673,10 @@ class PDBParser(StructureParserInterface):
         insert_code = f"{'':1}"
         occupancy = f"{1.00:>6.2f}"
 
-        temp_factor = f"{atom.b_factor:>6.2f}"
+        temp_factor = ' '*6
+        if atom.b_factor:
+            temp_factor = f"{atom.b_factor:>6.2f}"
+
         seg_id = f"{'':<4}"
         element = f"{atom.element:>2}"
         if atom.charge is None:
