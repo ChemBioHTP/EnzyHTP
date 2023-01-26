24336b7a2a8dc4f4994debfea5b3c3fa41a86306
shaoqz <shaoqz@icloud.com>
2023-01-25 18:12:02 -0600

    Update: add design of mutation pattern

--- enzy_htp/mutation/general.py
+++ enzy_htp/mutation/general.py
@@ -9,6 +9,7 @@ Author: Qianzhen (QZ) Shao <shaoqz@icloud.com>
 Date: 2022-10-24
 """
 
+from typing import List
 from enzy_htp.structure import Structure
 
 def mutate_stru(stru: Structure,
@@ -33,9 +34,30 @@ def mutate_stru(stru: Structure,
                   intact
                   (default is False since wild-type structure is expected to also available
                   in many applications)
+    Raises:
+        enzy_htp.core.exception.UnsupportedMethod if the supplied engine is not supported.
     Returns:
         the reference/copy of the changed structure (depends on the in_place value)
 
+    *Pattern Syntax:*
+        "section_1,section_2,section_3,..."
+        The pattern is composed by sections seperate by comma and each section can be one of the
+        format below:
+        1. direct indication            : XA###Y
+        2. random N mutation in a set   : r:N[mutation_set_patterns]
+        3. all mutation in a set: a     : a:[mutation_set_patterns]
+
+        The mutation_set_patterns is seperated by comma and each describes 2 things:
+        1. position_pattern: a set of positions
+                            (using the selection syntax in the selection module)
+        2. target_aa_pattern: a set of target mutations apply to all positions in the current set
+                            (using syntax in the target_aa_pattern module)
+        The two pattern are seperated by ":" and a mutation_set_patterns looks like:
+        "position_pattern_0:target_aa_pattern_0, ..."
+
+        Overall an example of pattern will be:
+        "RA154W, r:100[resi 289 around 4 and not resi 36:larger, proj(id 1000, id 2023, positive, 10):more_negative_charge]"
+        * here proj() is a hypothetical selection function
 
     Details:
         The problem have two parts: 1. assigning the mutation 2. predict the mutant structure.
@@ -108,6 +130,24 @@ def mutate_stru(stru: Structure,
             works mainly <5 AA change
     """
 
-# assign mutation (where to mutant to what)
+    # assign mutation (where to mutant to what)
+    # (pattern, stru) -> (MutaFlag)
+    mutation_flags = assign_mutation(stru, pattern)
 
-# deploy mutation (determine mutant structure)
+    # deploy mutation (determine mutant structure)
+    # stru -> stru
+
+def assign_mutation(stru: Structure, pattern: str) -> List[str]:
+    """
+    decode the user assigned {pattern} based on the {stru} and get a list of mutation flags
+    that direct indicate the mutation
+    Args:
+        stru: the target structure
+        pattern: the pattern that defines the mutation
+    Return:
+        a list of mutation_flag in the form of XA###Y. Where X and Y is the residue before and
+    after mutation, A### is the chain id and the residue index.
+    """
+    # decode the pattern
+    # write a new module about mutation pattern?
+
