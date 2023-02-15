9a8afcc5b896bcbfa73312a73037beaa472ab869
shaoqz <shaoqz@icloud.com>
2023-02-15 17:59:01 -0600

    update place holder

--- enzy_htp/mutation/mutation_pattern/target_aa_pattern.py
+++ enzy_htp/mutation/mutation_pattern/target_aa_pattern.py
@@ -3,3 +3,8 @@
 Author: QZ Shao <shaoqz@icloud.com>
 Date: 2023-01-26
 """
+from typing import Tuple, List
+from enzy_htp.structure import Structure
+
+def decode_target_aa_pattern(orig_resi: str, pattern: str):
+    """"""
