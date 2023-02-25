1fd3bb6da8e9c31807d5bdf39e4ce96be40f9adb
shaoqz <shaoqz@icloud.com>
2023-02-25 16:32:33 -0600

    Update: add some tool for typical enzyhtp run

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,6 +1,9 @@
+import os
 from subprocess import SubprocessError
 import pytest
 import helper
+
+DATA_DIR = f"{os.path.dirname(os.path.abspath(__file__))}/data_dir/"
 
 def test_run_cmd_fail_case():
     cmd_fail = 'cat abc'
@@ -12,3 +15,13 @@ def test_run_cmd_fail_case():
 def test_run_cmd_no_retry():
     cmd = 'squeue -u $USER'
     assert len(helper.run_cmd(cmd).stdout) != 0
+
+def test_check_complete_metric_run():
+    test_data_path = f"{DATA_DIR}Mutation.dat"
+    test_mutants = [
+        ['EA323R', 'EB773R', 'GA171R', 'GB621R'],
+        ['EA323R', 'EB773R', 'GA171H', 'GB621H'],
+        ['EA323R', 'EB773R', 'GA171K', 'GB621K'],
+        ['EA323R', 'EB773R', 'GA171D', 'GB621D']]
+    for mutant in test_mutants:
+        print(helper.check_complete_metric_run(mutant, test_data_path))
