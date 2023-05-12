157213d8ef5d67cb3c25a3bf480abbe817eedd35
shaoqz <shaoqz@icloud.com>
2023-05-12 08:46:49 -0500

    Update: update setup scheme

--- enzy_htp/mutation/__init__.py
+++ enzy_htp/mutation/__init__.py
@@ -6,8 +6,6 @@ Author: Chris Jurich <chris.jurich@vanderbilt.edu>
 Date: 2022-06-15
 """
 
-from .engine import mutate_pdb
-
 from .mutation import (
     Mutation,
     generate_from_mutation_flag,
@@ -17,8 +15,7 @@ from .mutation import (
     get_mutant_name_tag,
 )
 
-from .mutation_restrictions import (
-    MutationRestrictions,
-    restriction_object,
-    valid_restriction_dict,
+from .general import (
+    assign_mutant,
+    mutate_stru,
 )
