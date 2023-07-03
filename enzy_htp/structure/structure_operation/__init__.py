3cf54bf6491c1fac5947759e45fe4c24066752d8
shaoqz <shaoqz@icloud.com>
2023-07-03 15:10:27 -0500

    Update: change format based on comments

--- enzy_htp/structure/structure_operation/__init__.py
+++ enzy_htp/structure/structure_operation/__init__.py
@@ -3,5 +3,15 @@
 Author: Qianzhen (QZ) Shao, <shaoqz@icloud.com>
 Date: 2022-09-19
 """
-from .general import *
-from .residue import *
+from .general import (
+    remove_solvent,
+    remove_empty_chain,
+    remove_non_peptide,
+    update_residues,
+)
+from .residue import (
+    deprotonate_residue,
+    get_default_deproton_info,
+    remove_side_chain_mutating_atom,
+    check_res_topology_error,
+)
