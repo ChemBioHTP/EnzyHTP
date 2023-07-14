11fd98af6e3f47b0fc69790831224d5ffee59eaf
Sebastian Stull <stullsl@gw346.vampire>
2023-07-14 09:59:26 -0500

    Style changes to save fn. Add new export to pdb fn and test

--- enzy_htp/_interface/pymol_interface.py
+++ enzy_htp/_interface/pymol_interface.py
@@ -17,6 +17,7 @@ from enzy_htp.core import _LOGGER
 from enzy_htp.core import file_system as fs
 from enzy_htp._config.pymol_config import PyMolConfig, default_pymol_config
 from enzy_htp.structure import Structure, PDBParser
+from enzy_htp.chemical.residue import THREE_LETTER_AA_MAPPER
 
 # QZ:Env check for package type interface?
 try:
@@ -131,6 +132,7 @@ class PyMolInterface:
         # prints all rotamers and strains out; also saves each variation to a PDB file in scratch/.
         # can use for debugging purposes
         if debug:
+            mut_info = pos_key[0] + str(pos_key[1]) + THREE_LETTER_AA_MAPPER[target]
             for i in range(1, pymol_session.cmd.count_states() + 1):
                 pymol_session.cmd.wizard("mutagenesis")
                 pymol_session.cmd.do("refresh_wizard")
@@ -143,16 +145,48 @@ class PyMolInterface:
                 pymol_session.cmd.get_wizard().do_state(i)
                 pymol_session.cmd.frame(i)
                 pymol_session.cmd.get_wizard().apply()
-                self.save_to_stru(pymol_obj_name, pymol_session, True)
+                self.export_pymol_obj(pymol_obj_name, pymol_session, rotamer=i, mut=mut_info)
             return
 
         pymol_session.cmd.get_wizard().apply()
 
+    def export_pymol_obj(self, pymol_obj_name: str, 
+                        pymol_session: pymol2.PyMOL,
+                        if_retain_order: bool = True,
+                        rotamer: int = 0,
+                        mut: str = None) -> str:
+        """
+        Saves a PyMOL object to a PDB file.
+        Args:
+            pymol_obj_name: the name of the target enzyme in PyMOL.
+            pymol_session: the target PyMOL session.
+            in_order: if the saving should keep the order of the atoms in the original object.
+            rotamer: the current rotamer of the mutation.
+        Returns:
+            The path to the saved PDB file.
+        """
+        result_dir = eh_config["system.SCRATCH_DIR"]
+        fs.safe_mkdir(result_dir)
+        if mut and rotamer != 0:
+            pymol_outfile_path = fs.get_valid_temp_name(
+                    f"{result_dir}/pymol_output_{mut}_rotamer_{rotamer}.pdb")
+        elif mut and rotamer == 0:
+            pymol_outfile_path = fs.get_valid_temp_name(
+                    f"{result_dir}/pymol_output_{mut}.pdb")
+        else:
+            pymol_outfile_path = fs.get_valid_temp_name(
+                f"{result_dir}/pymol_output.pdb")
 
-    def save_to_stru(self, pymol_obj_name: str,
-                    pymol_session: pymol2.PyMOL,
-                    in_order: bool = True,
-                    keep_files = False) -> Structure:
+        if if_retain_order:
+            pymol_session.cmd.set("retain_order")
+        pymol_session.cmd.save(pymol_outfile_path, pymol_obj_name)
+
+        return pymol_outfile_path
+
+
+    def export_enzy_htp_stru(self, pymol_obj_name: str,
+                            pymol_session: pymol2.PyMOL,
+                            if_retain_order: bool = True) -> Structure:
         """
         Saves a PyMOL object to a Structure object.
         Args:
@@ -167,17 +201,14 @@ class PyMolInterface:
         temp_dir = eh_config["system.SCRATCH_DIR"]
         fs.safe_mkdir(temp_dir)
         pymol_outfile_path = fs.get_valid_temp_name(
-                f"{eh_config['system.SCRATCH_DIR']}/pymol_output.pdb")
+                f"{temp_dir}/pymol_output.pdb")
 
-        if in_order:
+        if if_retain_order:
             pymol_session.cmd.set("retain_order")
 
-        pymol_session.cmd.select(pymol_obj_name)
-        pymol_session.cmd.save(pymol_outfile_path, "(sele)")
+        pymol_session.cmd.save(pymol_outfile_path, pymol_obj_name)
         res = sp.get_structure(pymol_outfile_path)
-
-        if not keep_files:
-            fs.clean_temp_file_n_dir([temp_dir, pymol_outfile_path])
+        fs.clean_temp_file_n_dir([temp_dir, pymol_outfile_path])
         return res
         
         
