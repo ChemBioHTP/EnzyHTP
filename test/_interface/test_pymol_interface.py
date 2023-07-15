326071a6c844b46c176d356362ced48793ec24b3
shaoqz <shaoqz@icloud.com>
2023-07-14 19:04:23 -0500

    Bugfix: support allow_multichain_in_atom; add test

--- test/_interface/test_pymol_interface.py
+++ test/_interface/test_pymol_interface.py
@@ -94,7 +94,7 @@ def test_export_enzy_htp_stru():
         for new_atom, old_atom in zip(new_res.atoms, old_res.atoms):
             assert new_atom.coord == old_atom.coord
             assert new_atom.name == old_atom.name
-            
+      
 def test_export_pymol_obj():
     test_stru = PDBParser().get_structure(f"{DATA_DIR}KE_trun.pdb")
     pi = interface.pymol
@@ -108,7 +108,4 @@ def test_export_pymol_obj():
         for new_atom, old_atom in zip(new_res.atoms, old_res.atoms):
             assert new_atom.coord == old_atom.coord
             assert new_atom.name == old_atom.name
-
-def test_save_to_stru_1():
-    test_stru = PDBParser().get_structure("scratch/pymol_output.pdb")
 
