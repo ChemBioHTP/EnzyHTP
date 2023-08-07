4c365e201f88683d59c2287779bdd415ac769eec
Sebastian <sstull1842@gmail.com>
2023-08-07 18:46:33 -0400

    Comment out update_residues dispatch

--- enzy_htp/structure/structure_operation/general.py
+++ enzy_htp/structure/structure_operation/general.py
@@ -33,7 +33,8 @@ def remove_empty_chain(stru: Structure) -> Structure:
     original object.
     """
     ch: Chain
-    for ch in copy.copy(stru.chains):  # shallow copy to avoid iteration-deletion problem
+    # shallow copy to avoid iteration-deletion problem
+    for ch in copy.copy(stru.chains):
         if ch.is_empty():
             _LOGGER.debug(f"removing {ch}")
             ch.delete_from_parent()
@@ -50,7 +51,7 @@ def remove_non_peptide(stru: Structure) -> Structure:
     return stru
 
 
-@dispatch
+# @dispatch
 def update_residues(stru: Structure, ref_stru: Structure) -> Structure:
     """
     Update atoms and residue names to residues in the stru
@@ -61,7 +62,7 @@ def update_residues(stru: Structure, ref_stru: Structure) -> Structure:
     Returns:
         stru: the changed original structure
     """
-    #san check for ref_stru
+    # san check for ref_stru
     stru.is_idx_subset(ref_stru)
 
     # update residues in stru with correponding residue idxes in ref_stru
@@ -70,26 +71,28 @@ def update_residues(stru: Structure, ref_stru: Structure) -> Structure:
     for ref_res in ref_stru.residues:
         self_res = self_res_mapper[ref_res.key()]
         if self_res.name != ref_res.name:
-            _LOGGER.info(f"updating {self_res.key()} {self_res.name} to {ref_res.name}")
+            _LOGGER.info(
+                f"updating {self_res.key()} {self_res.name} to {ref_res.name}")
             self_res.name = ref_res.name
-        self_res.atoms = copy.deepcopy(ref_res.atoms)  # this will also set self_res as parent
+        # this will also set self_res as parent
+        self_res.atoms = copy.deepcopy(ref_res.atoms)
     stru.renumber_atoms()
     return stru
 
 
-@dispatch
-def update_residues(resi: Residue, ref_resi: Residue) -> Residue:  # pylint: disable=function-redefined
-    """
-    Update atoms and residue names to the single residue
-    (it doesnt matter if there are same in sequence)
-    Args:
-        resi: the target residue
-        ref_resi: the reference residue
-    Return:
-        the changed original residue
-    """
-    if resi.name != ref_resi.name:
-        _LOGGER.info(f"updating {resi.key()} {resi.name} to {ref_resi.name}")
-        resi.name = ref_resi.name
-    resi.atoms = copy.deepcopy(ref_resi.atoms)  # this will also set resi as parent
-    return resi
+# @dispatch
+# def update_residues(resi: Residue, ref_resi: Residue) -> Residue:  # pylint: disable=function-redefined
+#     """
+#     Update atoms and residue names to the single residue
+#     (it doesnt matter if there are same in sequence)
+#     Args:
+#         resi: the target residue
+#         ref_resi: the reference residue
+#     Return:
+#         the changed original residue
+#     """
+#     if resi.name != ref_resi.name:
+#         _LOGGER.info(f"updating {resi.key()} {resi.name} to {ref_resi.name}")
+#         resi.name = ref_resi.name
+#     resi.atoms = copy.deepcopy(ref_resi.atoms)  # this will also set resi as parent
+#     return resi
