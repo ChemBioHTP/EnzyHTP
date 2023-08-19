2aaa4fb10fd72b9da51b5e13c60e7b7964066042
Chris Jurich <chrisjurich@Chriss-MacBook-Pro-4.local>
2023-08-19 01:13:34 -0500

    local changes

--- enzy_htp/analysis/binding_energy.py
+++ enzy_htp/analysis/binding_energy.py
@@ -1,7 +1,9 @@
+"""TODO
 
-
-
-from typing import Union, List
+Author: Chris Jurich <chris.jurich@vanderbilt.edu>
+Date: 2023-08-17
+"""
+from typing import Union, List, Dict
 
 from numpy.typing import ArrayLike
 import numpy as np
@@ -12,20 +14,42 @@ from enzy_htp.structure import Structure, Atom
 
 from enzy_htp import interface, config
 
+
+
 def binding_energy( fname:str,
                     probe:str,                        
                     receptor:str,
+                    probe_charge:int=None,
+                    receptor_charge:int=None,
                     engine:str='xtb',
                     work_dir:str=None,
                     save_temp_files:bool=False,
                     **kwargs) -> float:
-
 
     """
     """
 
     if work_dir is None:
         work_dir = config['system.SCRATCH_DIR']        
+
+    #TODO(CJ): check here if the supplied method is supported
+
+    return BINDING_ENERGY_ENGINE[engine](
+        fname,probe,receptor,probe_charge,receptor_charge,work_dir,save_temp_files, **kwargs
+    )
+
+
+def xtb_binding_energy( fname:str,
+                        probe:str,                        
+                        receptor:str,
+                        probe_charge:int,
+                        receptor_charge:int,
+                        work_dir:str,
+                        save_temp_files:bool,
+                        **kwargs ) -> float:
+    """
+
+    """
 
     probe_xyz:str=f"{work_dir}/probe.xyz"
     receptor_xyz:str=f"{work_dir}/receptor.xyz"
@@ -37,20 +61,27 @@ def binding_energy( fname:str,
     session = interface.pymol.new_session()
 
     interface.pymol.create_cluster(session, fname, probe, outfile=probe_sdf,cap_strategy='CH3',work_dir=work_dir)
-    probe_charge:int=interface.bcl.calculate_formal_charge(probe_sdf)
+
+    if probe_charge is None:
+        probe_charge:int=interface.bcl.calculate_formal_charge(probe_sdf)
+    
     interface.pymol.general_cmd(session,[('delete','all')])
     interface.pymol.convert(session,probe_sdf,file_2=probe_xyz)
     
     interface.pymol.general_cmd(session,[('delete','all')])
-    interface.pymol.create_cluster(session, fname, receptor, outfile=receptor_sdf,cap_strategy='CH3',work_dir=work_dir)
-    receptor_charge:int=interface.bcl.calculate_formal_charge(receptor_sdf)
+    interface.pymol.create_cluster(session, fname, receptor, outfile=receptor_sdf, cap_strategy='CH3',work_dir=work_dir)
+   
+    if receptor_charge is None:
+        receptor_charge:int=interface.bcl.calculate_formal_charge(receptor_sdf)
+    
     interface.pymol.general_cmd(session,[('delete','all')])
     interface.pymol.convert(session,receptor_sdf,file_2=receptor_xyz)
 
 
     interface.pymol.general_cmd(session,[('delete','all')])
     interface.pymol.create_cluster(session, fname, f"({probe}) or ({receptor})", outfile=union_xyz,cap_strategy='CH3',work_dir=work_dir)
-    complete_charge=probe_charge+union_charge 
+    
+    union_charge=probe_charge+receptor_charge
 
     union_energy:float=interface.xtb.single_point(union_xyz, charge=union_charge)
     probe_energy:float=interface.xtb.single_point(probe_xyz, charge=probe_charge)
@@ -68,5 +99,9 @@ def binding_energy( fname:str,
 
     return union_energy - (probe_energy + receptor_energy)
 
-        
+
+BINDING_ENERGY_ENGINE:Dict={
+    "xtb":xtb_binding_energy,
+}       
+"""TODO(CJ)"""
 
