d32c80c81bc39e2e66cf9a442e80d7acb7e63fce
shaoqz <shaoqz@icloud.com>
2023-09-11 16:54:56 -0500

    Bugfix: add redirection of all logging handlers

--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,7 @@
 wappers and context manager defined for the program
 '''
 import os,sys
+import logging
 
 
 # def blockprint(redirect=os.devnull):
@@ -28,13 +29,25 @@ class HiddenPrints:
         self.redirect=redirect
 
     def __enter__(self):
-        self._original_stdout = sys.stdout
-        self._original_stderr = sys.stderr
-        self.redirect_file_obj = open(self.redirect, 'w')
-        sys.stdout = self.redirect_file_obj
-        sys.stderr = self.redirect_file_obj
+        self.original_stdout = sys.stdout
+        self.original_stderr = sys.stderr
+        self.redirect_stdout = open(self.redirect, 'w')
+        self.redirect_stderr = open(self.redirect, 'w')
+        sys.stdout = self.redirect_stdout
+        sys.stderr = self.redirect_stderr
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.redirect_file_obj.close()
-        sys.stdout = self._original_stdout
-        sys.stderr = self._original_stderr
+        self.redirect_stdout.close()
+        self.redirect_stderr.close()
+        sys.stdout = self.original_stdout
+        sys.stderr = self.original_stderr
+        # restore any logging handler that lead to redirect_io_obj
+        all_loggers = [logging.getLogger("")] + [j for i, j in logging.Logger.manager.loggerDict.items()]
+        for logger in all_loggers:
+            if hasattr(logger, "handlers"):
+                for handler in logger.handlers:
+                    if hasattr(handler, "stream"):
+                        if handler.stream is self.redirect_stdout:
+                            handler.stream = self.original_stdout
+                        if handler.stream is self.redirect_stderr:
+                            handler.stream = self.original_stderr
