18078d7fc21c927632457f2fc849a5f6dad8b3c5
shaoqz <shaoqz@icloud.com>
2023-09-19 21:40:18 -0500

    Update: create file structure for geometry

---
+++ enzy_htp/geometry/reaction.py
@@ -0,0 +1,12 @@
+"""This module defines functions related to reaction coordinates. Put everything related to
+reactions here. By reaction, it means anything related to bond change.
+This may include functions that:
+- Determine the current reaction coordinate value (which reaction state)
+- Modify the reaction coordinate (e.g.: change from reactant-product to TS or intermediate)
+- Define the reaction coordinate
+
+Science API:
+
+Author: Qianzhen (QZ) Shao <shaoqz@icloud.com>
+Date: 2023-8-1
+"""
