c33caf4f3ad01f3da98e61ff6afc2828050ff4b5
shaoqz <shaoqz@icloud.com>
2023-10-18 20:38:02 -0500

    Update: move test over

--- test/structure/structure_operation/test_connectivity.py
+++ test/structure/structure_operation/test_connectivity.py
@@ -3,17 +3,20 @@ Author: Qianzhen (QZ) Shao, <shaoqz@icloud.com>
 Date: 2023-03-20
 """
 import os
+import logging
 
-from enzy_htp import Structure
+from enzy_htp import _LOGGER
 from enzy_htp import PDBParser
+from enzy_htp.structure import Atom, Structure
 from enzy_htp.structure.structure_operation import init_connectivity
 
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.dirname(CURR_DIR)
 DATA_DIR = f"{CURR_DIR}/../../test_data"
+STRU_DATA_DIR = f"{CURR_DIR}/../data"
 sp = PDBParser()
 
-def test_init_connectivity():
+def test_init_connectivity(): #TODO: finish these test
     """test function works as expected"""
     pdb_file_path = f"{DATA_DIR}1Q4T_ligand_test.pdb"
     stru: Structure = sp.get_structure(pdb_file_path)
@@ -80,5 +83,40 @@ def test_init_connectivity_lv_6():
     test_stru = PDBParser().get_structure(
         f"{DATA_DIR}/diversed_stru/tyna_clean.pdb")
     init_connectivity(test_stru)
+
+# TODO 
+def test_init_connectivity_atom_missing_h(caplog):
+    """get connect with missing H atoms"""
+
+    original_level = _LOGGER.level
+    _LOGGER.setLevel(logging.DEBUG)
+
+    stru = PDBParser().get_structure(f"{STRU_DATA_DIR}1NVG.pdb")
+    atom1: Atom = stru["A"][0][0]  #N (Nter)
+    atom2: Atom = stru["A"][1][0]  #N
+    atom3: Atom = stru["A"][-1].find_atom_name("C")  #C (Cter)
+    init_connectivity(atom1)
+    init_connectivity(atom2)
+    init_connectivity(atom3)
+    assert len(atom1.connect) == 1
+    assert len(atom2.connect) == 2
+    assert len(atom3.connect) == 3
+    assert "missing connecting atom " in caplog.text
+
+    _LOGGER.setLevel(original_level)
+
+
+def test_init_connectivity_atom_w_h():
+    """get connect with no missing H atoms"""
+    stru = PDBParser().get_structure(f"{STRU_DATA_DIR}1Q4T_peptide_protonated.pdb")
+    atom1: Atom = stru["A"][0][0]  #N (Nter)
+    atom2: Atom = stru["A"][1][0]  #N
+    atom3: Atom = stru["A"][-1].find_atom_name("C")  #N (Cter)
+    init_connectivity(atom1)
+    init_connectivity(atom2)
+    init_connectivity(atom3)
+    assert len(atom1.connect) == 4
+    assert len(atom2.connect) == 3
+    assert len(atom3.connect) == 3
 
 
