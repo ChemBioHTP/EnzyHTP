f5a05a149e944ee6e57f0ecdf2c7536952b62163
shaoqz <shaoqz@icloud.com>
2023-10-28 02:50:48 -0500

    Update: add StructureEnsemble class

--- enzy_htp/geometry/demo_main.py
+++ enzy_htp/geometry/demo_main.py
@@ -32,16 +32,3 @@ step_1: MolDynStep = interface.amber.MolDynStep(stru, ntb=1, ntp=0,... , cluster
 step_2: MolDynStep = copy.deepcopy(step_1).set(ntb=2, ntp=1,... , cluster_job=True)
 step_3: MolDynStep = copy.deepcopy(interface.amber.prod_step).set(stru, temp=2, cluster_job=True)
 all_kinds_of_md_output = md_simulation(parms=parms, steps=[step_1, step_2, step_3], repeat=10) # could there be a better design; check engine consistency inside
-
-class StructureEnsemble:
-    def __init__(self, topology, top_format, coordinate, coord_format) -> None:
-        # topology is a structure object with empty coord?
-        # coordinate: List[List[Tuple[float, float, float]]]
-        # both default stays in file?
-        pass
-
-    def get_stru_list(self) -> List[Structure]:
-        pass
-
-class MolDynStep(ABC): # abstract class defined in geometry? maybe in interface a file called requirements.py
-    pass
