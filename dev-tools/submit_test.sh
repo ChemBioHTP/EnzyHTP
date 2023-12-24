4de0d62d6aa10441e5aae3592595908c734117d6
shaoqz <shaoqz@icloud.com>
2023-12-24 16:53:08 -0600

    Update: add tools for submitting tests

---
+++ dev-tools/submit_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=debug
+#SBATCH --job-name=devtest_enzy_htp
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=30:00
+#SBATCH --account=yang_lab_csb
+
+source /home/shaoq1/bin/amber_env/amber22.sh
+source ~/bin/miniconda3/bin/activate new_EnzyHTP
+
+python -u -m pytest test/_interface/test_amber_interface.py::test_amber_md_step_run -s > ./test/pytest.out
