c3bce6e268136e75fac9fe17d1f1421af032ba24
shaoqz <shaoqz@icloud.com>
2024-01-23 21:20:08 -0600

    Update: finished parse_format and test

--- test/core/test_fortran_helper.py
+++ test/core/test_fortran_helper.py
@@ -7,37 +7,38 @@ import enzy_htp.core.fortran_helper as fh
 
 def test_parse_single_format():
     """as name"""
-    test = ""
-    assert fh.parse_single_format(test) == ()
+    assert fh.parse_single_format("20A4") == (20, str, 4, 0)
 
-# TODO
-def test_parse_format_uppercase():
+    assert fh.parse_single_format("5E16.8") == (5, float, 16, 8)
+
+    assert fh.parse_single_format("10I8") == (10, int, 8, 0)
+
+    assert fh.parse_single_format("I8") == (1, int, 8, 0)
+
+def test_parse_format():
     """Testing that the parse_format() method works correctly for uppercase inputs."""
-    assert fh.parse_format('%FORMAT(20A4)') == (str, 4)
+    assert fh.parse_format('FORMAT(20A4)') == [(20, str, 4, 0)]
 
-    assert fh.parse_format('%FORMAT(5E16.8)') == (float, 16)
+    assert fh.parse_format('FORMAT(5E16.8)') == [(5, float, 16, 8)]
 
-    assert fh.parse_format('%FORMAT(10I8)') == (int, 8)
+    assert fh.parse_format('FORMAT(10I8)') == [(10, int, 8, 0)]
 
+    assert fh.parse_format('FORMAT(20a4)') == [(20, str, 4, 0)]
 
-def test_parse_format_lowercase():
-    """Testing that the parse_format() method works correctly for lowercase inputs."""
-    assert fh.parse_format('%FORMAT(20a4)') == (str, 4)
-
-    assert fh.parse_format('%FORMAT(5e16.8)') == (float, 16)
-
-    assert fh.parse_format('%FORMAT(10i8)') == (int, 8)
-
+    assert fh.parse_format('FORMAT(20A4, 5E16.8, 10I8)') == [
+        (20, str, 4, 0),
+        (5, float, 16, 8),
+        (10, int, 8, 0)
+    ]
 
 def test_parse_format_bad_input():
     """Testing that the parse_format() method returns the correct (None,-1) for bad inputs."""
 
+    assert fh.parse_format('') == (-1, None, -1, -1)
 
-    assert fh.parse_format('') == (None, -1)
+    assert fh.parse_format('20a4') == (-1, None, -1, -1)
 
-    assert fh.parse_format('20a4') == (None, -1)
+    assert fh.parse_format('%FORMAT(20a4)') == (-1, None, -1, -1)
 
-    assert fh.parse_format('FORMAT(20a4)') == (None, -1)
-
-    assert fh.parse_format('%FORMAT(20a4') == (None, -1)
+    assert fh.parse_format('FORMAT(20a4') == (-1, None, -1, -1)
 
