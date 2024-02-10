062c7b97aa1df6cf49984939a84111f76e9973b1
shaoqz <shaoqz@icloud.com>
2024-02-10 17:44:26 -0600

    Update: finished ef_hotspot and test

--- enzy_htp/mutation/mutation_pattern/position_pattern.py
+++ enzy_htp/mutation/mutation_pattern/position_pattern.py
@@ -4,7 +4,14 @@ sequence position selection pattern.
 Author: QZ Shao <shaoqz@icloud.com>
 Date: 2023-01-26
 """
-from typing import List
+import re
+import numpy as np
+from plum import dispatch
+from typing import Iterable, List, Tuple
+
+import enzy_htp.core.math_helper as mh
+from enzy_htp.core.general import split_but_brackets
+from enzy_htp.core.logger import _LOGGER
 from enzy_htp.structure import Structure, Residue
 from enzy_htp.structure.structure_selection import select_stru
 
@@ -21,20 +28,119 @@ def decode_position_pattern(stru: Structure, pattern: str, if_name: bool = False
                example: "resi 254 around 5"
             2. a pattern starts with "$" will be recognized to use
                an internal EnzyHTP function to selected positions.
-               example: "$cone(B.254.CAE, B.254.H2, 10)" means residues
+               example: "$ef_hotspot(B.254.CAE, B.254.H2, (0,10))" means residues
                within a cone region that formed by points that have angle
                <= 10 degree than vector from B.254.CAE to B.254.H2.
 
     Returns:
         (chain_id, resi_idx) to indicate a mutation position
         ((chain_id, resi_idx), resi_name)  if_name=True"""
-    selection_obj = select_stru(stru, pattern)
-    result_residue: Residue = selection_obj.involved_residues
-    result_residue = filter(lambda i: i.is_canonical() or i.is_modified(),
-                            result_residue)  # because it is impossible and dont make sense to mutate non-polypeptide
+    # 1. dispatch pattern type
+    pattern = pattern.strip()
+    if pattern.startswith("$"):
+        result_residue = decode_builtin_function(stru, pattern)
+    else:
+        result_residue = decode_pymol_selection(stru, pattern)
+
     if if_name:
         result = [(x.key(), x.name) for x in result_residue]
     else:
         result = [x.key() for x in result_residue]
 
     return result
+
+def decode_builtin_function(stru: Structure, pattern: str) -> Iterable[Residue]:
+    """decode the position pattern in builtin function format.
+    example: '$ef_hotspot('B.254.CAE', 'B.254.H2', (0,10))'"""
+    pattern_format = r"\$(\w+)\((.+)+\)"
+    funcname, args = re.match(pattern_format, pattern).groups()
+    
+    func = PATTERN_BUILTIN_FUNCTION_MAPPER[funcname]
+    args = [eval(i.strip()) for i in split_but_brackets(args, ",")]
+
+    return func(stru, *args)
+
+
+def decode_pymol_selection(stru: Structure, pattern: str) -> Iterable[Residue]:
+    """decode the position pattern in pymol selection format.
+    example: 'resi 254 around 5'"""
+    selection_obj = select_stru(stru, pattern)
+    result_residue: Residue = selection_obj.involved_residues
+    result_residue = filter(lambda i: i.is_canonical() or i.is_modified(),
+                            result_residue)  # because it is impossible and dont make sense to mutate non-polypeptide
+    return result_residue
+
+@dispatch
+def ef_hotspot(stru: Structure, atom_1: str, atom_2: str, cutoff: Tuple) -> List[Residue]:
+    """find mutation sites that are most impactful to
+    the internal electric field of the enzyme in terms of
+    its stablization effect on a bond (defined by {atom_1, atom_2}).
+    The sites are calculated to be within a cone region defined by
+    <the angle between the dipole and the vector points from the
+    dipole center to residue in interval {cutoff}>
+    Args:
+        stru:
+            the target enzyme as a pre-reaction complex
+        atom_1, atom_2:
+            the target bond defined by 2 atoms (atom_1 -> atom_2)
+        cutoff:
+            the degree cutoff value for finding the mutations site.
+    Returns:
+        positions as a list of Residue()"""
+    result = []
+    # init
+    low, high = cutoff
+    atom_1 = np.array(stru.get(atom_1).coord)
+    atom_2 = np.array(stru.get(atom_2).coord)
+    p2 = (atom_1 + atom_2) / 2
+    p1 = atom_2
+    # collect
+    for res in stru.amino_acids:
+        # 1. find mass center
+        p3 = np.array(res.geom_center)
+        # 2. calc angle and collect
+        angle = mh.get_angle(p1, p2, p3)
+        if angle <= high and angle >= low:
+            _LOGGER.debug(f"found {res.key_str} - {angle}°")
+            result.append(res)
+    return result
+
+@dispatch
+def ef_hotspot(stru: Structure, dipole_vec: Tuple, dipole_center: Tuple, cutoff: Tuple) -> List[Residue]:
+    """find mutation sites that are most impactful to the internal 
+    electric field of the enzyme in terms of its stablization effect
+    on a dipole (defined by {dipole_vec, dipole_center}).
+    The sites are calculated to be within a cone region defined by
+    <the angle between the dipole and the vector points from the
+    dipole center to residue in interval {cutoff}>
+    Args:
+        stru:
+            the target enzyme as a pre-reaction complex
+        dipole_vec, dipole_center:
+            the target bond dipole defined by a vector and the center location.
+        cutoff:
+            the degree cutoff value for finding the mutations site.
+    Returns:
+        positions as a list of Residue()"""
+    result = []
+    # init
+    low, high = cutoff
+    p1 = np.array(dipole_center)
+    d1 = np.array(dipole_vec)
+    # collect
+    for res in stru.amino_acids:
+        # 1. find mass center
+        p2 = np.array(res.geom_center)
+        d2 = p2 - p1
+        # 2. calc angle and collect
+        angle = mh.get_angle_vec(d1, d2)
+        if angle <= high and angle >= low:
+            _LOGGER.debug(f"found {res.key_str} - {angle}°")
+            result.append(res)
+    return result
+    
+
+PATTERN_BUILTIN_FUNCTION_MAPPER = {
+    "ef_hotspot" : ef_hotspot,
+}
+
--- test/mutation/mutation_pattern/test_position_pattern.py
+++ test/mutation/mutation_pattern/test_position_pattern.py
@@ -3,10 +3,12 @@ Author: QZ Shao <shaoqz@icloud.com>
 Date: 2023-02-17
 """
 
+import logging
 import os
 
 from enzy_htp import PDBParser
-import enzy_htp.mutation.mutation_pattern.api as m_p
+from enzy_htp.core.logger import _LOGGER
+import enzy_htp.mutation.mutation_pattern.position_pattern as m_p
 
 CURR_FILE = os.path.abspath(__file__)
 CURR_DIR = os.path.dirname(CURR_FILE)
@@ -34,3 +36,32 @@ def test_decode_position_pattern_dimer():
                                                                              ('A', 269), ('A', 264), ('A', 390), ('A', 270), ('A', 265),
                                                                              ('A', 391), ('A', 271), ('A', 266), ('A', 388),
                                                                              ('B', 719)])  # note ligand wont be included
+
+def test_decode_builtin_function():
+    """test the function use a made up builtin_function pattern for KE"""
+
+    test_pdb = f"{DATA_DIR}KE_07_R7_2_S.pdb"
+    test_stru = sp.get_structure(test_pdb)
+    test_pattern = "$ef_hotspot('B.254.CAE', 'B.254.H2', (170,180))"
+
+    existing_level = _LOGGER.level
+    _LOGGER.setLevel(logging.DEBUG)
+
+    result = m_p.decode_builtin_function(test_stru, test_pattern)
+
+    _LOGGER.setLevel(existing_level)
+
+    assert set([res.key_str for res in result]) == set(["A.21", "A.24", "A.22", "A.228"])
+
+def test_decode_builtin_function_vec():
+    """test the function use a made up builtin_function pattern for KE.
+    test the dispatch on using vector"""
+    existing_level = _LOGGER.level
+    _LOGGER.setLevel(logging.DEBUG)
+
+    test_pdb = f"{DATA_DIR}KE_07_R7_2_S.pdb"
+    test_stru = sp.get_structure(test_pdb)
+    test_pattern = "$ef_hotspot((-0.373, -0.1285, 0.369), (22.665,-2.5785,-52.461), (170,180))"
+    result = m_p.decode_builtin_function(test_stru, test_pattern)
+    _LOGGER.setLevel(existing_level)
+    assert set([res.key_str for res in result]) == set(["A.21", "A.24", "A.22", "A.228"])
