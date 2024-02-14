bda08c16280422083c5823cfac17bd1494c832e5
shaoqz <shaoqz@icloud.com>
2024-02-13 20:53:15 -0600

    clean up

--- enzy_htp/_interface/handle_types/thermostability_engine.py
+++ enzy_htp/_interface/handle_types/thermostability_engine.py
@@ -6,40 +6,32 @@ modules of different external software.
 Author: Qianzhen (QZ) Shao <shaoqz@icloud.com>
 Date: 2024-02-13
 """
-from abc import ABC, abstractmethod
-from typing import Union, List, Tuple
+from abc import abstractmethod
+from typing import List, Tuple
 
-from ..base_interface import BaseInterface
 from .modeling_engine import ModelingEngine, ModelingResultEgg
 
-from enzy_htp.electronic_structure import ElectronicStructure
+from enzy_htp.mutation_class import Mutation
 from enzy_htp.structure import Structure
 from enzy_htp.core.job_manager import ClusterJob
 
 class ddGResultEgg(ModelingResultEgg):
-    """This class defines the format of QM result eggs.
-    These eggs are QM result place holders before the calculation.
-    An example of eggs is file paths, so in Gaussian's case, it could be
-    the .out file path etc. In general it should be some info to deduce
-    where the result will be.
-    The motivation of having this class is when running QM through ARMer
-    the jobs.
-    In most cases, the concrete class is a dataclass"""
+    """see ModelingResultEgg"""
     pass
 
 class ddGFoldEngine(ModelingEngine):
-    """the qm engine that performs single point calculation"""
+    """the ddg fold calculation engine that performs ddg fold calculation"""
     @abstractmethod
-    def make_job(self, stru: Structure) -> Tuple[ClusterJob, ddGResultEgg]:
-        """the method that make a ClusterJob that runs the QM"""
+    def make_job(self, stru: Structure, mutant_space: List[Mutation]) -> Tuple[ClusterJob, ddGResultEgg]:
+        """the method that make a ClusterJob that runs the ddg fold calculation"""
         pass
 
     @abstractmethod
-    def run(self, stru: Structure) -> ElectronicStructure:
-        """the method that runs the QM"""
+    def run(self, stru: Structure) -> float:
+        """the method that runs the ddg fold calculation"""
         pass
 
     @abstractmethod
-    def translate(self, egg: ddGResultEgg) -> ElectronicStructure:
+    def translate(self, egg: ddGResultEgg) -> float:
         """the method convert engine specific results to general output"""
         pass
