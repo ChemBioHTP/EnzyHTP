c5791786fb8f40572723b0ebebaadb347e98d1e3
Chris Jurich <chris.jurich@vanderbilt.edu>
2024-04-18 17:30:33 -0500

    more updates and documentation

--- enzy_htp/analysis/spi.py
+++ enzy_htp/analysis/spi.py
@@ -1,6 +1,14 @@
-"""
+"""Calculates the substrate positioning index (spi or spi metric) which uses solvent accessible surface area (SASA)
+to calculate a ratio between the ligand and defined pocket. In other words, spi = SASA_ligand / SASA_pocket. Functionality 
+is accessed through spi_metric(), which is  overloaded with the below signatures:
 
-DOI: https://doi.org/10.1021/acs.jpclett.3c02444
+    + spi_metric( StructureEnsemble, Ligand, List[Residue] ) -> List[float]
+    + spi_metric( StructureEnsemble, Ligand, str) -> List[float]
+    + spi_metric( Structure, Ligand, str) -> float
+
+The most used API will typically be spi_metric( StructureEnsemble, Ligand, List[Residue] ). To get the List[Residue], consider
+using enzy_htp.structure.structure_operation.closest_n_residues(). Note that all SASA calculations are done using pymol's
+python API. Citation for spi: DOI:https://doi.org/10.1021/acs.jpclett.3c02444.
 
 Author: Chris Jurich <chris.jurich@vanderbilt.edu>
 Date: 2024-04-18
@@ -20,15 +28,18 @@ def spi_metric(
     ligand : Ligand,
     pocket_sele: str 
 ) -> List[float]:
-    """
+    """Calculates the spi metric for a StructureEnsemble using a pymol-formatted pocket sele str. 
+    Note: The pocket_sele expression is applied to each Structure(), so it is recommended to use a selection which specifies
+    individual Residue()'s as the expression will be re-evaluated for each Structure() geometry.
+
     Args:
-        stru:
-        ligand:
-        pocket_sele:
+        stru: The StructureEnsemble() to analyze.
+        ligand: The Ligand() which will be the numerator in the spi metric.
+        pocket_sele: A pymol-formatted sele str which defines the denominator in the spi metric.
 
     Returns:
+        A List[float] of len(StructureEnsemble.structures), with each value being a different spi metric for the nth Structure(). 
     """
-
     
     if isinstance(stru, Structure):
         #TODO(CJ): put some kind of thing here
@@ -48,19 +59,21 @@ def spi_metric(
     ligand : Ligand,
     pocket: List[Residue],
 ) -> List[float]:
-    """
-    """
+    """Calculates the spi metric for a StructureEnsemble using a List[Residue] that define the denominator of the 
+    metric. This is the PREFERRED method for calculating spi.
 
-    
-    if isinstance(stru, Structure):
-        #TODO(CJ): put some kind of thing here
-        stru = stru.from_single_stru( stru )
+    Args:
+        stru: The StructureEnsemble() to analyze.
+        ligand: The Ligand() which will be the numerator in the spi metric.
+        pocket: A List[Residue] which serves as the pocket or SASA denominator of the spi metric.
 
+    Returns:
+        A List[float] of len(StructureEnsemble.structures), with each value being a different spi metric for the nth Structure(). 
+    """
     pocket_sele = " or ".join(map(
         lambda rr: f"( chain {rr.parent.name} and resi {rr.idx} )",
         pocket
     ))
-    print(pocket_sele)                
 
     result = list()
     for ss in stru.structures:
@@ -76,7 +89,17 @@ def spi_metric(
 def spi_metric( stru: Structure, 
             ligand: Ligand,
             pocket_sele:str ) -> float:
-    """
+    """Calculates the spi metric for a single Structure using a pymol-formatted pocket sele str. 
+    Note: The pocket_sele expression is applied to the Structure(), so it is recommended to use a selection which specifies
+    individual Residue()'s.
+
+    Args:
+        stru: The Structure() to analyze.
+        ligand: The Ligand() which will be the numerator in the spi metric.
+        pocket_sele: A pymol-formatted sele str which defines the denominator in the spi metric.
+
+    Returns:
+        A List[float] of len(StructureEnsemble.structures), with each value being a different spi metric for the nth Structure(). 
     """
 
     (lig_chain, lig_idx) = ligand.key()
