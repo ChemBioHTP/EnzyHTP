a5b61af7e5f2a9e1c52a8b7fe63cac1102d1c813
Chris Jurich <chrisjurich@Chriss-MacBook-Pro-7.local>
2024-05-28 01:32:17 -0500

    continuing to work on the loop modelling PR

--- enzy_htp/preparation/remedy.py
+++ enzy_htp/preparation/remedy.py
@@ -2,6 +2,7 @@
 or some side-chain heavy atoms.
 
 Author: Qianzhen (QZ) Shao <shaoqz@icloud.com>
+Author: Chris Jurich <chris.jurich@vanderbilt.edu>
 Date: 2022-09-22
 """
 import requests
@@ -17,7 +18,6 @@ def add_missing_residues( stru: Structure,
                                 missing_residues:List[SeqRes], 
                                 method:str='modeller', 
                                 work_dir:str=None,
-                                inplace:bool=True
                                 ) -> Union[None,Structure]:
 
 
@@ -36,10 +36,22 @@ def add_missing_residues( stru: Structure,
 
     if not missing_residues:
         return #TODO(CJ): check if I need to return a cpy or not 
+
+    func =  RESIDUE_ADDER_MAPPER.get( method )
+
+    if func is None:
+        #TODO(CJ): add the error here about the supplied method not being supported
+        pass
+
     interface.modeller.add_missing_residues( stru, missing_residues, work_dir=work_dir, inplace=inplace)
 
 def identify_missing_residues( code:str ) -> List[SeqRes]:
     """
+    Args:
+        code: The 4-letter PDB code to look up.
+
+    Returns:
+        A List[SeqRes] of the missing residues in the given PDB code.
     """
 
     #TODO(CJ): add in some checking about the supplkied PDB code. 
@@ -68,8 +80,20 @@ def identify_missing_residues( code:str ) -> List[SeqRes]:
         three_letter:str=ll[15:18].strip()
         chain_name:str=ll[19].strip()
         idx:int=int(ll[21:26])
-        result.append(SeqRes(model=1,chain=chain_name,idx=idx,name=three_letter,missing=True, seq_idx=None))
+        result.append(SeqRes(
+            model=1,
+            chain=chain_name,
+            idx=idx,
+            name=three_letter,
+            missing=True,
+            seq_idx=None))
 
     result.sort()
 
     return result
+
+RESIDUE_ADDER_MAPPER:Dict[str, Callable] = {
+    'modeller':interface.modeller.add_missing_residues,
+    'rosetta':interface.rosetta.add_missing_residues
+}
+"""TODO(CJ)"""
