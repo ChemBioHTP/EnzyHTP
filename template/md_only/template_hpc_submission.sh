203c09288cc8b57ab05c6716b0f1ea7c1bf3b118
shaoqz <shaoqz@icloud.com>
2025-01-15 16:25:20 -0600

    Update: update the submission script

--- template/md_only/template_hpc_submission.sh
+++ template/md_only/template_hpc_submission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=qkst_template
-#SBATCH --account=xxx
+#SBATCH --account=xxx # CHANGE this to your account
 #SBATCH --partition=production
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -12,13 +12,13 @@
 # Gaussian (for formchk)
 module load Gaussian/16.B.01
 # AmberTool (for tleap antechamber parmchk)
-source /home/shaoq1/bin/amber_env/amber-accre.sh
+source /sb/apps/amber22/amber.sh
 # Multiwfn
 module load GCC/6.4.0-2.28  OpenMPI/2.1.1
 export KMP_STACKSIZE=200000000
 export Multiwfnpath=/home/shaoq1/bin/Multiwfn_3.7_bin_Linux_noGUI
 export PATH=$PATH:$Multiwfnpath
 # EnzyHTP
-source ~/bin/miniconda3/bin/activate enzy_htp
+source ~/bin/miniconda3/bin/activate enzy_htp # CHANGE this to your command that activate the environment. (Note: if you are using conda from module load. You would need to copy the conda init section from ~/.bashrc to here before running conda activate xxx)
 
 python -u template_main.py > template_main.py.out 2>&1
