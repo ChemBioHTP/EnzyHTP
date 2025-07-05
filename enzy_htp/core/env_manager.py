477b50fd897b93b584d00c120e286f5ad14d2657
Chris Jurich <chrisjurich@Chriss-MacBook-Pro-10.local>
2025-07-05 17:50:23 -0400

    removed TODO lines

--- enzy_htp/core/env_manager.py
+++ enzy_htp/core/env_manager.py
@@ -166,8 +166,9 @@ class EnvironmentManager:
                 are some unrepeatable error thus allow taking next retry earlier.)
                 (Unit: s)
             quiet_fail:
-                #TODO(CJ)
-            log_level: the logging level that non-error/warning loggings in this function goes to.
+                Quiet failing mode allows system commands to have a non-zero return code without escalating the error.
+            log_level: 
+                the logging level that non-error/warning loggings in this function goes to.
 
         Returns:
             return the CompletedProcess object
