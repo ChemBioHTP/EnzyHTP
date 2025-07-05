55260f899ac8bd15f5e733a5a7f27676ee50e63d
Chris Jurich <chrisjurich@Chriss-MacBook-Pro-10.local>
2025-07-05 18:56:10 -0400

    local

--- enzy_htp/structure/structure_cluster/structure_cluster.py
+++ enzy_htp/structure/structure_cluster/structure_cluster.py
@@ -1,4 +1,11 @@
-#TEST
+"""Implements a StrutureCluster class which is meant to be essentially a List[Structure] with some additional information about RMSD 
+differences between each member in the List. This file contains the StructureCluster class definition as well as the cluster_structures() 
+method, a free function which creates StructureCluster()'s from a raw List[Structure].
+
+Author: Chris Jurich <chris.jurich@vanderbilt.edu>
+Date: 2025-07-05
+"""
+
 from typing import List
 
 import numpy as np
@@ -8,9 +15,15 @@ from enzy_htp import interface
 
 
 class StructureCluster:
+    """Represents a geometric cluster of Structure's that with low RMSDs between constituent members. Functionally, 
+    it is a List[Structure] with more information about member-member RMSDs stored in an RMSD matrix.
 
-
+    Attributes:
+        structures_: The List[Structure] containing members.
+        rmsd_matrix_: The List[List[float]] matrix with RMSD's between all members. Has dimensions of len(structures_)xlen(structures_)
+    """
     def __init__(self, rmsd_matrix, first_stru:Structure=None ):
+        """Simple constructor which takes the RMSD matrix and optionally the first Structure to add."""
         self.structures_ = list()
         self.rmsd_matrix_ = rmsd_matrix
 
