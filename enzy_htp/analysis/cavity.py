fe253016b01c60a8e52b4a3ad22b8f90d228303b
SwordJack <49801226+SwordJack@users.noreply.github.com>
2025-07-16 00:41:52 -0500

    Add `ensemble_cavity_volumes` function to `analysis.cavity`.

--- enzy_htp/analysis/cavity.py
+++ enzy_htp/analysis/cavity.py
@@ -15,8 +15,9 @@ import numpy as np
 import numpy.typing as npt
 import pyvista as pv
 
-from enzy_htp import interface, _LOGGER, PDBParser, Residue
-from enzy_htp.structure import Structure, Residue, Cavity
+from enzy_htp import interface, _LOGGER, PDBParser
+from enzy_htp.structure import Structure, StructureEnsemble, Residue, Cavity
+from enzy_htp.structure.structure_selection import select_stru
 from enzy_htp import config as eh_config
 from enzy_htp.core import file_system as fs
 
@@ -59,3 +60,97 @@ def identify_stru_cavities(stru: Structure,
     cavities = CAVITY_IDENTIFICATION_METHODS[engine](stru=stru, work_dir=work_dir, **kwargs)
     
     return cavities
+
+def _choose_cavity(cavity_list: List[Cavity], 
+        composing_residues: List[Residue] = None, 
+        contain_ligand: str = None, 
+        target_cavity: Cavity = None
+    ) -> Tuple[Cavity, int]:
+    """
+    Selects the most similar cavity to the focus cavity based on residue key overlap.
+    
+    Args:
+        cavity_list: List of Cavity objects to compare. All cavities should be in the same Structure instance.
+        composing_residues (List[Residue], optional): A list of Residue instances composing the cavity. Mutually exclusive with `contain_ligand` or `target_cavity`.
+        contain_ligand (str, optional): The PyMOL-formatted pattern selecting the ligand in the cavity. Mutually exclusive with `target_cavity` or `composing_residues`.
+        target_cavity (Cavity, optional): The target cavity to track throughout the ensemble (by most overlapped residues). Mutually exclusive with `composing_residues` or `contain_ligand`.
+    
+    Returns:
+        result (Tuple[Cavity, int]): 
+            * Cavity: The Cavity object from cavity_list with highest residue key overlap with focus_cavity
+            * int: max_overlap value.
+    """
+    if sum(x is not None for x in (composing_residues, contain_ligand, target_cavity)) != 1:
+        raise ValueError("The `composing_residues`, `contain_ligand` and `target_cavity` are mutually exclusive to each other.")
+    
+    if (contain_ligand):
+        stru = cavity_list[0].stru
+        ligand_selection = select_stru(stru=stru, pattern=contain_ligand)
+        focus_point = np.array(ligand_selection.atoms[0].coord)
+        for cavity in cavity_list:
+            if cavity.contains(focus_point):
+                return cavity, 1
+            else:
+                continue
+        return None, 0
+    else:
+        focus_residue_keys = set()
+        cavity_similarity = dict()
+        if (target_cavity):
+            focus_residue_keys = set(resi.key() for resi in (target_cavity.boundary_residues + target_cavity.inner_residues))
+        if (composing_residues):
+            focus_residue_keys = set(resi.key() for resi in composing_residues)
+        for i, cavity in enumerate(cavity_list):
+            cavity_residue_keys = set(resi.key() for resi in (cavity.boundary_residues + cavity.inner_residues))
+            cavity_similarity[i] = len(cavity_residue_keys.intersection(focus_residue_keys))
+        return cavity_list[max(cavity_similarity, key=cavity_similarity.get)], max(cavity_similarity.values())
+
+def ensemble_cavity_volumes(
+        stru_esm: StructureEnsemble, 
+        composing_residues: List[Residue] = None, 
+        contain_ligand: str = None, 
+        target_cavity: Cavity = None, 
+        frame_0_based: bool = True,
+        **kwargs) -> List[float]:
+    """Caculate cavity volumes from a StructureEnsemble instance.
+    The cavity should be selected by a list of Residue instances, a ligand string (PyMOL formatted), or a Cavity instance.
+    
+    Args:
+        stru_esm (StructureEnsemble): The StructureEnsemble instance to calculate cavities from.
+        composing_residues (List[Residue], optional): A list of Residue instances composing the cavity. Mutually exclusive with `contain_ligand` or `target_cavity`.
+        contain_ligand (str, optional): The PyMOL-formatted pattern selecting the ligand in the cavity. Mutually exclusive with `target_cavity` or `composing_residues`.
+        target_cavity (Cavity, optional): The target cavity to track throughout the ensemble (by most overlapped residues). Mutually exclusive with `composing_residues` or `contain_ligand`.
+        frame_0_based (bool, optional): Indicate if the cavity selection input is formulated with the frame 0 structure to get target cavity.
+
+    Returns:
+        volumes (List[float]): The list of cavity volume value from the ensemble.
+    """
+    if sum(x is not None for x in (composing_residues, contain_ligand, target_cavity)) != 1:
+        raise ValueError("The `composing_residues`, `contain_ligand` and `target_cavity` are mutually exclusive to each other.")
+    esm_cavities: List[Cavity] = list()
+    structure_0 = stru_esm.structure_0
+
+    target_cavity_confirmed = None
+    if frame_0_based:   # Confirm the target cavity if `frame_0_based=True`.
+        frame_0_cavities = identify_stru_cavities(stru=structure_0, **kwargs)
+        target_cavity_confirmed, _ = _choose_cavity(cavity_list=frame_0_cavities, 
+            composing_residues=composing_residues, contain_ligand=contain_ligand, target_cavity=target_cavity)
+        if (target_cavity_confirmed is None):
+            raise ValueError("Unable to identify target cavity from frame 0 structure.")
+    else:
+        target_cavity_confirmed = target_cavity
+    
+    for stru_frame in stru_esm.structures(remove_solvent=True):     # Iterate over the ensemble.
+        frame_cavities = identify_stru_cavities(stru=stru_frame, **kwargs)
+        cavity, max_overlap = _choose_cavity(cavity_list=frame_cavities, 
+            composing_residues=composing_residues, contain_ligand=contain_ligand, target_cavity=target_cavity_confirmed)
+        esm_cavities.append(cavity)
+        continue
+
+    volumes = list()
+    for cavity in esm_cavities:
+        if (cavity is None):
+            volumes.append(0.0)
+        else:
+            volumes.append(cavity.volume)
+    return volumes
