861ede97206fed09290a5be84df4ca5ca7b66625
shaoqz <shaoqz@icloud.com>
2025-08-05 23:22:49 -0500

    Update: clean up that run_prepgen

--- enzy_htp/_config/amber_config.py
+++ enzy_htp/_config/amber_config.py
@@ -302,6 +302,7 @@ class AmberConfig(BaseConfig):
             "add_pdb",
             "ante-MMPBSA.py",
             "parmed",
+            "prepgen",
             self.HARDCODE_MMMPBSA_MPI_ENGINE,
         ]
 
