84123bba133f6f00e739f617e5083e2d58eda224
Chris Jurich <chris.jurich@vanderbilt.edu>
2025-08-25 12:28:07 -0500

    resolved preparation

--- enzy_htp/preparation/__init__.py
+++ enzy_htp/preparation/__init__.py
@@ -7,6 +7,7 @@ Author: Chris Jurich <chris.jurich@vanderbilt.edu>
 Date: 2022-03-19
 """
 
+from .pdb_line import PDBLine, read_pdb_lines
 from .protonate import protonate_stru
 from .clean import remove_solvent, remove_hydrogens
 from .reactive_docking import dock_reactants
@@ -15,6 +16,7 @@ from .seed_ligand import (
     seed_with_coordinates,
     seed_with_transplants,
     seed_with_constraints,
+    seed_using_phosphates,
     seed_with_pdb_structure
 )
 from .remedy import (
