43f0e7c4c6dd42954eb5de17c8bc5c6249f943f0
shaoqz <shaoqz@icloud.com>
2025-09-02 17:48:27 -0500

    Update: collect and update workflow_app

--- enzy_htp/workflow_app/aclmt_main.py
+++ enzy_htp/workflow_app/aclmt_main.py
@@ -10,7 +10,6 @@ import glob
 import itertools
 import os
 from pathlib import Path
-import re
 import sys
 import numpy as np
 import pickle
@@ -30,7 +29,6 @@ from enzy_htp.core import _LOGGER
 from enzy_htp.core.general import save_obj, load_obj, save_func_to_main
 import enzy_htp.core.math_helper as mh
 import enzy_htp.core.file_system as fs
-from enzy_htp import interface as ei
 
 def run_shrapnel(
         tasks: Dict,
@@ -69,7 +67,7 @@ def run_shrapnel(
             "task" in this list:
                 {   
                 "wt" : [wt_path, ...],
-                "mutants" : mutant_pattern (or a List[List[Mutation]]),
+                "mutants" : mutant_pattern,
                 "constraints" : [md_constraint, ...],
                 "ncaa_chrgspin" : {"RES" : (0,1),...},
                 },
@@ -221,7 +219,7 @@ def run_shrapnel(
     # signal handling in case hitting the walltime while waiting for child jobs to finish
     _sig_handler_partial = partial(_sig_handler, child_jobs=child_jobs, child_jobs_checkpoint=child_job_checkpoint)
     signal.signal(signal.SIGUSR1, _sig_handler_partial)
-    signal.signal(signal.SIGTERM, _sig_handler_partial)
+    # signal.signal(signal.SIGTERM, _sig_handler_partial)
 
     jobs_remain = ClusterJob.wait_to_array_end_plus(child_jobs, shrapnel_check_period, shrapnel_child_array_size)
     # update child jobs
@@ -270,7 +268,7 @@ def _child_main(
     from enzy_htp.preparation import protonate_stru, remove_hydrogens
     from enzy_htp.mutation import mutate_stru
     from enzy_htp.geometry import equi_md_sampling
-    from enzy_htp.analysis import ele_field_strength_at_along, ddg_fold_of_mutants, rmsd, binding_energy
+    from enzy_htp.analysis import ele_field_strength_at_along, ddg_fold_of_mutants
     from enzy_htp import interface
     from enzy_htp.mutation_class import Mutation
     from enzy_htp.structure import StructureConstraint, Structure, Atom, StructureEnsemble
@@ -367,7 +365,6 @@ def _child_main(
                 stru = mutant_stru,
                 param_method = param_method,
                 cluster_job_config = gpu_job_config,
-                dont_freeze_bb_in_min=True,
                 prod_constrain=task_constraints,
                 prod_time=md_length,
                 record_period=md_length*0.01,
@@ -379,53 +376,12 @@ def _child_main(
             result_dict[task_uid]["trajs"] = trajs
             save_obj(result_dict, result_path)
 
-        # metrics specific
-        mut_data = {
-            "ef" : [],
-            "rmsd" : [],
-            "mmpbsa" : [],
-        }
-        for replica_esm in trajs:
-            # EF
-            replica_ef = []
-            atom_1 = mutant_stru.get("C.290.C1")
-            atom_2 = mutant_stru.get("C.290.I1")
-            ef_region_pattern = "resi 1-288"
-            for traj_stru in replica_esm.structures(remove_solvent=True):
-                # 5. get dEF
-                field_strength = ele_field_strength_at_along(
-                    traj_stru, atom_1, atom_2, region_pattern=ef_region_pattern)
-                replica_ef.append(field_strength)
-            mut_data["ef"].append(replica_ef)
-
-            # RMSD
-            replica_rmsd = rmsd(
-                replica_esm, 
-                region_pattern="resi 7+11+27+40+41+43+165+168+169+170+199+210+211+289 and (not elem H)",
-            )
-            mut_data["rmsd"].append(replica_rmsd)
-
-            # MMPBSA
-            replica_mmpbsa = binding_energy(
-                replica_esm,
-                ligand="resi 290",
-                method="mmpbsa_amber",
-                cluster_job_config=cpu_job_config,
-            )
-            mut_data["mmpbsa"].append(replica_mmpbsa)
-
-        result_dict[task_uid].update(mut_data)
-        save_obj(result_dict, result_path)
-
     # final san check
     for task in tasks:
         task_uid = task["uid"]
         assert task_uid in result_dict
         # assert "ddg_fold" in result_dict[task_uid]
         assert "trajs" in result_dict[task_uid]
-        assert "ef" in result_dict[task_uid]
-        assert "rmsd" in result_dict[task_uid]
-        assert "mmpbsa" in result_dict[task_uid]
 
 def _make_child_job(
         grp_id: int,
@@ -479,16 +435,16 @@ def _sig_handler(signum, frame, *, child_jobs, child_jobs_checkpoint):
     sys.exit(0)  
 
 def resubmit_child_jobs(child_job_list: list, child_job_checkpoint: str, 
-                        shrapnel_check_period: int, shrapnel_child_array_size: int):
+                        shrapnel_check_period: int, shrapnel_child_array_size: int, shrapnel_dir: str):
     """Resubmit a specific list of children jobs.
     This function is handy when you have part of the child jobs failed after the main script finishes.
     Always dump resubmitted child jobs to a new file."""
     child_jobs: List[ClusterJob] = load_obj(child_job_checkpoint) # NOTE that this will cause source code change in this script cannot effect the content of those existing jobs. (e.g.: partition etc.)
-    child_jobs_mapper = {i.sub_dir.removeprefix("./shrapnel/") : i for i in child_jobs}
+    child_jobs_mapper = {i.sub_dir.removeprefix(shrapnel_dir) : i for i in child_jobs}
 
     new_child_jobs = [child_jobs_mapper[group_name] for group_name in child_job_list]
     new_child_job_checkpoint = Path(child_job_checkpoint).with_suffix(".new.pickle")
-    new_child_job_checkpoint = fs.get_valid_temp_name(new_child_job_checkpoint)
+    new_child_job_checkpoint = fs.get_valid_temp_name(str(new_child_job_checkpoint))
     
     _sig_handler_partial = partial(_sig_handler, child_jobs=new_child_jobs, child_jobs_checkpoint=new_child_job_checkpoint)
     signal.signal(signal.SIGUSR1, _sig_handler_partial)
@@ -552,62 +508,17 @@ def _determine_task_md_finish(task: dict, results: dict, md_parallel_runs: int,)
     else:
         return True
 
-def fix_broken_result_file(
-        target_dirs: List[str], 
-        child_result_fname: str = "results.pickle",
-        child_task_fname: str = "tasks.pickle",
-    ):
-    """fix broken results.pickle file"""
-    for d in target_dirs:
-        result_fname = Path(d) / child_result_fname
-        task_fname = Path(d) / child_task_fname
-        try:
-            load_obj(result_fname)
-        except (pickle.UnpicklingError, EOFError):
-            pass
-        else:
-            _LOGGER.error("try to fix a none broken result pickle. Please exam yourself.")
-            raise ValueError
-        
-        # fix broken ones
-        tasks = load_obj(task_fname)
-        result = {}
-        # find existing trajs
-        task_dir = Path(d).glob("task_*/")
-        for t in task_dir:
-            traj_nc = (t / "MD").glob("rep_0*")
-            traj_nc = sorted(traj_nc, key=lambda x: x.stem)
-            traj_nc = traj_nc[-1]
-            traj_nc_idx = traj_nc.stem.removeprefix("rep_0")
-            traj_nc = traj_nc / "prod_npt.nc"
-            traj_prmtop = t / "MD" / f"amber_parm{traj_nc_idx}.prmtop"
-            task_idx = int(t.stem.removeprefix("task_"))
-            task_info = tasks[task_idx]
-            task_uid = task_info["uid"]
-            print(traj_prmtop, traj_nc)
-            result[task_uid] = {
-                "trajs" : [ei.amber.load_traj(
-                    prmtop_path=traj_prmtop,
-                    traj_path=traj_nc
-                )]
-            }
-        save_obj(result, result_fname)
-
-
-
-
 def main():
-    """shrapnel-like dir creation and submission"""
+    """shrapnel-like dir creation and submission for"""
     # region: Input
     tasks = [
         {   
-        "wt" : ["wt/aclHMT-IPI-SAH.pdb"],
-        "mutants" : load_obj("stable_mutants.pickle"),
-        "constraints" : [
-            partial(stru_cons.create_distance_constraint,"B.289.S1", "C.290.C1", 3.5),
-            partial(stru_cons.create_angle_constraint,"B.289.S1", "C.290.C1", "C.290.I1", 180.0),            
-            ],
-        "ncaa_chrgspin" : {"SAH" : (0,1), "IPI" : (0,1)},
+        # "wt" : ["wt/athmt.pdb"],
+        "wt" : ["wt/aclmt.pdb"],
+        # "mutants" : "a:1[resi 8+11+15+35+127+128+131+160+202:all not self]",
+        "mutants" : load_obj("mutants_hammers_aclmt.pickle"),
+        "constraints" : [],
+        "ncaa_chrgspin" : {"SAH" : (0,1)},
         },
     ]
     # temp (will deprocate)
@@ -634,95 +545,114 @@ def main():
             "walltime" : "3-00:00:00",}}
     # endregion
 
-    run_shrapnel(
-        tasks = tasks,
-        chain_sync_list=chain_sync_list, chain_index_mapper=chain_index_mapper,
-        # MD
-        md_length=50.0,
-        md_parallel_runs=1,
-        # ARMer
-        shrapnel_child_job_config = shrapnel_child_job_config,
-        shrapnel_cpujob_config = shrapnel_cpujob_config,
-        shrapnel_gpujob_config = shrapnel_gpujob_config,
-        # shrapnel
-        shrapnel_child_array_size = 100,
-        shrapnel_groups = 100,
-        shrapnel_gpu_partition_mapper = {
-            (0, 10) : "nvidia_rtx_a6000:1",
-            (11, 20) : "nvidia_rtx_a6000:1",
-            (21, 60) : "nvidia_titan_x:1",
-            (61, 90) : "nvidia_geforce_rtx_2080_ti:1",
-            # (71, 80) : "nvidia_h100_80gb:1", # NOTE h100 not working (cuda version not compitable)
-            (91, 95) : "nvidia_rtx_a6000:1", # NOTE a100 not working (used to work)
-            (96, 100) : "nvidia_rtx_a6000:1",
-        },
-    )
-
-    # resubmit_child_jobs(
-    #     child_job_list=[
-    #         "group_44",
-    #         "group_47",
-    #         "group_48",
-    #         "group_50",
-    #         "group_51",
-    #         "group_53",
-    #         "group_54",
-    #         "group_55",
-    #         "group_56",
-    #         "group_57",
-    #         "group_58",
-    #         "group_59",
-    #         "group_60",
-    #         "group_61",
-    #         "group_62",
-    #         "group_63",
-    #         "group_64",
-    #         "group_65",
-    #         "group_66",
-    #         "group_67",
-    #         "group_68",
-    #         "group_69",
-    #         "group_70",
-    #         "group_71",
-    #         "group_72",
-    #         "group_73",
-    #         "group_74",
-    #         "group_75",
-    #         "group_76",
-    #         "group_77",
-    #         "group_78",
-    #         "group_79",
-    #         "group_80",
-    #         "group_88",
-    #     ],
-    #     child_job_checkpoint="shrapnel_child_jobs.pickle",
-    #     shrapnel_check_period = 120,
-    #     shrapnel_child_array_size = 50,
+    # run_shrapnel(
+    #     tasks = tasks,
+    #     chain_sync_list=chain_sync_list, chain_index_mapper=chain_index_mapper,
+    #     # MD
+    #     md_length=50.0,
+    #     md_parallel_runs=1,
+    #     # ARMer
+    #     shrapnel_child_job_config = shrapnel_child_job_config,
+    #     shrapnel_cpujob_config = shrapnel_cpujob_config,
+    #     shrapnel_gpujob_config = shrapnel_gpujob_config,
+    #     # shrapnel
+    #     work_dir = "./shrapnel_aclmt",
+    #     shrapnel_child_array_size = 100,
+    #     shrapnel_groups = 100,
+    #     shrapnel_gpu_partition_mapper = {
+    #         (0, 10) : "nvidia_rtx_a6000:1",
+    #         (11, 20) : "nvidia_titan_x:1", # NOTE a100 not working
+    #         (21, 70) : "nvidia_geforce_rtx_2080_ti:1",
+    #         (71, 80) : "nvidia_rtx_a6000:1", # NOTE h100 not working
+    #         (81, 100) : "nvidia_titan_x:1",
+    #     },
     # )
 
-    # child_jobs: List[ClusterJob] = load_obj("shrapnel_child_jobs.pickle")
-    # for j in child_jobs:
-    #     replaced = j.sub_script_str.replace("nvidia_a100_80gb", "nvidia_rtx_a6000")
-    #     if replaced != j.sub_script_str:
-    #         print(j.sub_dir.removeprefix(""))
-    #         j.sub_script_str = replaced
-    # save_obj(child_jobs, "shrapnel_child_jobs_updated.pickle")
+    resubmit_child_jobs(
+        child_job_list=[
+            "group_11",
+            "group_13",
+            "group_15",
+            "group_16",
+            "group_17",
+            "group_18",
+            "group_19",
+            "group_20",
+            "group_21",
+            "group_22",
+            "group_23",
+            "group_24",
+            "group_25",
+            "group_26",
+            "group_28",
+            "group_29",
+            "group_30",
+            "group_31",
+            "group_32",
+            "group_35",
+            "group_38",
+            "group_39",
+            "group_40",
+            "group_42",
+            "group_43",
+            "group_44",
+            "group_45",
+            "group_46",
+            "group_47",
+            "group_48",
+            "group_49",
+            "group_50",
+            "group_52",
+            "group_54",
+            "group_56",
+            "group_57",
+            "group_58",
+            "group_59",
+            "group_60",
+            "group_61",
+            "group_62",
+            "group_65",
+            "group_66",
+            "group_67",
+            "group_68",
+            "group_69",
+            "group_70",
+            "group_83",
+            "group_84",
+            "group_85",
+            "group_86",
+            "group_87",
+            "group_89",
+            "group_90",
+            "group_91",
+            "group_93",
+            "group_95",
+            "group_96",
+            "group_97",
+            "group_98",
+            "group_99",
+        ],
+        child_job_checkpoint="shrapnel_child_jobs.pickle",
+        shrapnel_check_period = 120,
+        shrapnel_child_array_size = 50,
+        shrapnel_dir="./shrapnel_aclmt/",
+    )
 
     # unfinished = (detect_child_job_progress(
     #     md_parallel_runs=1,
     #     print_unfinished_only=True,
     # ))
-    # unfinished_idx = set([int(i.removeprefix("./shrapnel/group_")) for i in unfinished])
-    # running_idx = {94}
-    # running_md_idx = {0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,22,24,25,27,28,29,30,31,36,40,41,45,49,54,59,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,90,91,92,93,94,95,96,97,98,99}
+    # unfinished_idx = set([int(i.removeprefix("./shrapnel_aclmt/group_")) for i in unfinished])
+    # running_idx = {
+    #     0 ,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10,12,14,27,33,34,36,37,41,51,53,55,63,64,71,72,73,74,75,76,77,78,79,80,81,82,88,92,94
+    # }
+    # running_md_idx = {
+    #     34,92,27,53,36,2,8,0,1,6,5,3,55,7,4,10,9,12,88,76,37,74,71,73,72,77,75,33,78,79,41,80,64,51,63,81,14,82,94
+    # }
     # print(unfinished_idx - running_idx)
     # print(unfinished_idx - running_md_idx)
-
-    # fix_broken_result_file([
-    #     "./shrapnel/group_13",
-    #     "./shrapnel/group_15",
-    #     "./shrapnel/group_18",
-    # ])
+    # print(running_idx - running_md_idx)
+    # print(running_md_idx - running_idx)
 
 if __name__ == "__main__":
     main()
